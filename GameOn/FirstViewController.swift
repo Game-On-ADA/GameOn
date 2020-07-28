18400f2764835dbdf40e93acb48cffb5e58307a2
Mas Rasid <rasid517@gmail.com>
2020-07-29 06:40:14 +0700

    Initial Commit

---
+++ GameOn/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  GameOn
+//
+//  Created by Muhammat Rasid Ridho on 29/07/20.
+//  Copyright © 2020 Muhammat Rasid Ridho. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
