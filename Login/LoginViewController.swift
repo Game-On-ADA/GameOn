cc85dd650ed051d634781dd876942f303c9c1f8c
MilhamAt <M477ashiddiq@gmail.com>
2020-08-11 22:52:33 +0700

    one

---
+++ Login/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  GameOn
+//
+//  Created by Muhammad Ilham Ashiddiq Tresnawan on 09/08/20.
+//  Copyright © 2020 Muhammat Rasid Ridho. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
